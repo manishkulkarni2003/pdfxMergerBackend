7081b41114215c78e170f96c7c21b7351ab58571
manishkulkarni2003 <mk3304939@gmail.com>
2025-04-20 22:36:55 +0530

    frontend for the pdf merger application

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+ 
+
+  
+
+  return (
+    <>
+    <div>
+      Hello Pdf Meger
+    </div>
+    </>
+
+  );
+}
+
+export default App;
+
+//Lets do this with spring boot
+//backend Setup Done now Git setuo and css setup left to do
