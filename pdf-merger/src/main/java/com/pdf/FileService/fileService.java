79de825fa7f2702e0f3d125310693f70483bdf39
manishkulkarni2003 <mk3304939@gmail.com>
2025-04-20 22:52:22 +0530

    Moved backend files to the 'backend' folder

--- pdf-merger/src/main/java/com/pdf/FileService/fileService.java
+++
@@ -1,8 +0,0 @@
-package com.pdf.FileService;
-
-import com.pdf.Fileentity.fileRecord;
-
-public interface fileService {
-	fileRecord savefile(fileRecord record);
-	
-}
--- pdf-merger/src/main/java/com/pdf/fileRepository/fileRecordRepo.java
+++
@@ -1,9 +0,0 @@
-package com.pdf.fileRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pdf.Fileentity.fileRecord;
-
-public interface fileRecordRepo extends JpaRepository<fileRecord,Long> {
-
-}
